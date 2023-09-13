e25b7c518b9bd4731bd7ac3177d757dab2a7a402
ozod akromov <akromovasadbek@gmail.com>
2023-09-13 13:29:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const images = document.querySelectorAll('.slider .images img')
 const prev = document.querySelector('.slider .btns .prev')
 const next = document.querySelector('.slider .btns .next')
-
-
+const content = document.querySelector('.images')
+// const content = document.querySelectorAll('.content .images')
 let index = 0
 
 // Next Btn
@@ -26,3 +26,22 @@ prev.addEventListener('click', function() {
     images[index].classList.add('active')
 })
 
+
+content.addEventListener('click', function() {
+    images[index].classList.remove('active')
+    index++
+    if(index === images.length) {
+        index = 0
+    }
+    images[index].classList.add('active')
+})
+
+content.addEventListener('contextmenu', function(e) {
+    e.preventDefault()
+    images[index].classList.remove('active')
+    if(index === 0) {
+        index = 4
+    }
+    index--
+    images[index].classList.add('active')
+})
